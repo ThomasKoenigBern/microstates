18bb69cc525e4c3079ff5623c8af5285396a24ba
Delara Aryan <aryan@usc.edu>
2022-04-28 13:23:22 -0700

    update Hartigan to use trace(W)

--- eeg_Hartigan.m
+++ eeg_Hartigan.m
@@ -1,11 +1,11 @@
-function H = eeg_Hartigan(W, ClusterNumbers, nsamples)
+function H = eeg_Hartigan(TW, ClusterNumbers, nsamples)
     numClustSolutions = length(ClusterNumbers);
 
     %% Compute Hartigan index for each cluster solution
     H = zeros(numClustSolutions, 1);
     for c = 1:numClustSolutions
         nc = ClusterNumbers(c);     % number of clusters
-        Wratio = (trace(W{c})/trace(W{c+1})) - 1;
+        Wratio = TW(c)/TW(c+1) - 1;
         n = nsamples(c);
         H(c) = Wratio*(n-nc-1);
     end
