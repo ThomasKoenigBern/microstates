377a00cbeb7b4888363c48f6fb0b531b61796e83
delara-aryan <dearyan@chla.usc.edu>
2022-10-19 13:20:28 -0700

    change label and color updating to reflect new sort order

--- UpdateMicrostateLabels.m
+++ UpdateMicrostateLabels.m
@@ -20,9 +20,34 @@ function [NewLabels, NewColors] = UpdateMicrostateLabels(OldLabels,TemplateLabel
 
     else
         NewLabels(1:nTemplateLabels) = TemplateLabels;
-        NewLabels((nTemplateLabels+1):nOldLabels) = OldLabels(ToKeep);
+
+        % Delara 10/19/22 change: keep old labels if they were from sorting
+        % by other template maps. otheriwse, update the generic labels to
+        % reflect their new position in the order
+        ToKeepIndices = find(ToKeep);
+        for i=1:numel(ToKeepIndices)
+            labelIndex = ToKeepIndices(i);
+            if ~contains(OldLabels(labelIndex), sprintf('MS_%i', nOldLabels))
+                NewLabels{nTemplateLabels+i} = OldLabels{labelIndex};
+            else
+                NewLabels{nTemplateLabels+i} = sprintf('MS_%i.%i', nOldLabels, nTemplateLabels+i);
+            end
+        end
+%         NewLabels((nTemplateLabels+1):nOldLabels) = OldLabels(ToKeep);
+
         if nargin > 3
             NewColors(1:nTemplateLabels ,:) = TemplateColor;
-            NewColors((nTemplateLabels+1:nOldLabels),:) = OldColor(ToKeep,:);
+
+            % same change for colors
+            defaultColors = lines(nOldLabels);
+            for i=1:numel(ToKeepIndices)
+                labelIndex = ToKeepIndices(i);
+                if ~contains(OldLabels(labelIndex), sprintf('MS_%i', nOldLabels))
+                    NewColors(nTemplateLabels+i, :) = OldColor(labelIndex, :);
+                else
+                    NewColors(nTemplateLabels+i, :) = defaultColors(nTemplateLabels+i, :);
+                end
+            end
+%             NewColors((nTemplateLabels+1:nOldLabels),:) = OldColor(ToKeep,:);
         end
     end
