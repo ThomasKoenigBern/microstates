6d75f3fb88705618e2375048a0ca759bb14eb842
delara-aryan <dearyan@chla.usc.edu>
2023-03-13 12:20:48 -0700

    remove old files

--- SetFittingParameters.m
+++ SetFittingParameters.m
@@ -1,113 +1,162 @@
-%SetFittingParameters() Choose fitting parameters
-%
-% Usage: 
-%   >> params = SetFittingParameters(PossibleNs, params, AddChannelFlag)
-%
-% Where: - PossibleNs is an array of possible microstate class numbers
-%        - params is a structure with the fitting parameters
-%          (see <a href="matlab:helpwin AssignMStates">AssignMStates</a> for details
-% Output:
-%        - params:   The fitting parameters chosen
-%
-% Author: Thomas Koenig, University of Bern, Switzerland, 2016
-%
-% Copyright (C) 2016 Thomas Koenig, University of Bern, Switzerland, 2016
-% thomas.koenig@puk.unibe.ch
-%
-% This program is free software; you can redistribute it and/or modify
-% it under the terms of the GNU General Public License as published by
-% the Free Software Foundation; either version 2 of the License, or
-% (at your option) any later version.
-%
-% This program is distributed in the hope that it will be useful,
-% but WITHOUT ANY WARRANTY; without even the implied warranty of
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-% GNU General Public License for more details.
-%
-% You should have received a copy of the GNU General Public License
-% along with this program; if not, write to the Free Software
-% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-%
-function params = SetFittingParameters(PossibleNs, params, AddChannelFlag)
+function FitPar = SetFittingParameters(PossibleNs, FitPar, funcName, AddOptions)
+    
+    if nargin < 4;  AddOptions = false; end
 
-    if nargin < 3;  AddChannelFlag = false;        end
-    if nargin < 2;  params = [];        end
-    if nargin < 1;  PossibleNs = 2:10;  end    
-  
-    choice = '';
-    if(~isempty(PossibleNs))
-        for i = 1:numel(PossibleNs)
-            choice = [choice sprintf('%i Classes|',PossibleNs(i))];
-        end
-    
-        choice(end) = [];
-    end
-    
-    if ~isfield(params,'b');        params.b          = 0;    end
-    if ~isfield(params,'lambda');    params.lambda    = 0.3;  end
-    if ~isfield(params, 'PeakFit');  params.PeakFit   = true; end
-    if ~isfield(params, 'nClasses'); params.nClasses  = min(PossibleNs); end
-    if ~isfield(params, 'BControl'); params.BControl  = true; end
-    if ~isfield(params, 'Rectify');  params.Rectify   = false; end
-    if ~isfield(params, 'Normalize');params.Normalize = false; end
-    
-    idx = find(params.nClasses == PossibleNs);
-    if(isempty(idx));   idx = 1;    end
-    
-    if AddChannelFlag == true
-        EnableFitPars = 'on';
-    else
-        EnableFitPars = 'off';
+    % Initial validation
+    [FitPar, FitParDefaults] = checkFitPar(funcName, PossibleNs, FitPar);
+
+    guiElements = {};
+    guiGeom = {};
+    guiGeomV = []; 
+
+    % Add all fitting parameters that were filled in with default values as
+    % gui elements
+    if contains('nClasses', FitParDefaults)
+        classChoices = sprintf('%i Classes|', PossibleNs);
+        classChoices(end) = [];
+
+        guiElements = [guiElements, ...
+            {{ 'Style', 'text', 'string', 'Number of classes', 'fontweight', 'bold'}} ...
+            {{ 'Style', 'listbox', 'string', classChoices, 'Value', 1, 'Tag', 'nClasses'}}];
+        guiGeom = [guiGeom [1 1]];
+        guiGeomV = [guiGeomV 3];
     end
 
-    if (isempty(PossibleNs))
-        res = inputgui( 'geometry', {1 1 1 [1 1] [1 1] 1 1}, 'uilist', { ...
-            { 'Style', 'checkbox', 'string' 'Fitting only on GFP peaks' 'tag' 'PeakFit'    ,'Value', params.PeakFit }  ...
-            { 'Style', 'checkbox', 'string' 'Remove potentially truncated microstates' 'tag' 'PeakFit'    ,'Value', params.BControl }  ...
-            { 'Style', 'text', 'string', 'Label smoothing (window = 0 for no smoothing)', 'fontweight', 'bold', 'HorizontalAlignment', 'center'} ...
-            { 'Style', 'text', 'string', 'Label smoothing window (ms)', 'fontweight', 'bold'  } ...
-            { 'Style', 'edit', 'string', num2str(params.b) 'tag' 'SmoothWindow'} ...
-            { 'Style', 'text', 'string', 'Non-Smoothness penalty', 'fontweight', 'bold'  } ...
-            { 'Style', 'edit', 'string', num2str(params.lambda) 'tag' 'lambda' } ... 
-            { 'Style', 'checkbox', 'string','Rectify microstate fit' 'tag' 'Rectify','Value',params.Rectify,'Enable',EnableFitPars} ... 
-            { 'Style', 'checkbox', 'string','Normalize microstate fit' 'tag' 'Normalize','Value',params.Normalize,'Enable',EnableFitPars } ... 
-            },'title','Choose microstate fitting parameters');
-    else
-        res = inputgui( 'geometry', {[1 1] 1 1 1 1 [1 1] [1 1] 1 1}, 'geomvert', [3 1 1 1 1 1 1 1 1],  'uilist', { ...
-            { 'Style', 'text', 'string', 'Number of classes', 'fontweight', 'bold'  } ...
-            { 'style', 'listbox', 'string', choice, 'Value', idx}...
-            { 'Style', 'text', 'string', ''} ...
-            { 'Style', 'checkbox', 'string' 'Fitting only on GFP peaks' 'tag' 'PeakFit'    ,'Value', params.PeakFit }  ...
-            { 'Style', 'checkbox', 'string' 'Remove potentially truncated microstates' 'tag' 'PeakFit'    ,'Value', params.BControl }  ...
-            { 'Style', 'text', 'string', 'Label smoothing (window = 0 for no smoothing)', 'fontweight', 'bold', 'HorizontalAlignment', 'center'} ...
-            { 'Style', 'text', 'string', 'Label smoothing window (ms)', 'fontweight', 'bold'  } ...
-            { 'Style', 'edit', 'string', num2str(params.b) 'tag' 'SmoothWindow'} ...
-            { 'Style', 'text', 'string', 'Non-Smoothness penalty', 'fontweight', 'bold'  } ...
-            { 'Style', 'edit', 'string', num2str(params.lambda) 'tag' 'lambda' } ... 
-            { 'Style', 'checkbox', 'string','Rectify microstate fit' 'tag' 'Rectify','Value',params.Rectify,'Enable',EnableFitPars} ... 
-            { 'Style', 'checkbox', 'string','Normalize microstate fit' 'tag' 'Normalize','Value',params.Normalize,'Enable',EnableFitPars } ... 
-            },'title','Choose microstate fitting parameters');
+    if contains('PeakFit', FitParDefaults)
+        if contains('nClasses', FitParDefaults)
+            guiElements = [guiElements, {{ 'Style', 'text', 'string', ''}}];
+            guiGeom = [guiGeom 1];
+            guiGeomV = [guiGeomV 1];
+        end
+
+        guiElements = [guiElements, ...
+            {{ 'Style', 'checkbox', 'string' 'Fitting only on GFP peaks' 'tag' 'PeakFit','Value', FitPar.PeakFit }}];
+        guiGeom = [guiGeom 1];
+        guiGeomV = [guiGeomV 1];
     end
-    
-    if isempty(res)
-        params = [];
-        return
-    else
-        if (isempty(PossibleNs))
-            params.PeakFit = res{1};
-            params.BControl = res{2};
-            params.b       = str2double(res{3});
-            params.lambda  = str2double(res{4});
-            params.Rectify = res{5};
-            params.Normalize = res{6};
-        else
-            params.nClasses = PossibleNs(res{1});
-            params.PeakFit = res{2};
-            params.BControl = res{3};
-            params.b       = str2double(res{4});
-            params.lambda  = str2double(res{5});
-            params.Rectify = res{6};
-            params.Normalize = res{7};
+
+    if contains('BControl', FitParDefaults)
+        guiElements = [guiElements, ...
+            {{ 'Style', 'checkbox', 'string' 'Remove potentially truncated microstates' 'tag' 'BControl','Value', FitPar.BControl }}];
+        guiGeom = [guiGeom 1];
+        guiGeomV = [guiGeomV 1];
+    end
+
+    if (contains('b', FitParDefaults) || contains('lambda', FitParDefaults)) && ~FitPar.PeakFit
+        guiElements = [guiElements, ...
+            {{ 'Style', 'text', 'string', 'Label smoothing (window = 0 for no smoothing)', 'fontweight', 'bold', 'HorizontalAlignment', 'center'}}];
+        guiGeom = [guiGeom 1];
+        guiGeomV = [guiGeomV 1];
+
+        if contains('b', FitParDefaults)
+            guiElements = [guiElements, ...
+                {{ 'Style', 'text', 'string', 'Label smoothing window (ms)', 'fontweight', 'bold' }} ...
+                {{ 'Style', 'edit', 'string', num2str(FitPar.b) 'tag' 'b'}}];
+            guiGeom = [guiGeom [1 1]];
+            guiGeomV = [guiGeomV 1];
+        end
+
+        if contains('lambda', FitParDefaults)
+            guiElements = [guiElements, ...
+                {{ 'Style', 'text', 'string', 'Non-Smoothness penalty', 'fontweight', 'bold' }} ...
+                {{ 'Style', 'edit', 'string', num2str(FitPar.lambda) 'tag' 'lambda' }}];
+            guiGeom = [guiGeom [1 1]];
+            guiGeomV = [guiGeomV 1];
         end
     end
+
+    if AddOptions
+        if contains('Rectify', FitParDefaults)
+            guiElements = [guiElements, ...
+                {{ 'Style', 'checkbox', 'string','Rectify microstate fit' 'tag' 'Rectify','Value',FitPar.Rectify}}];
+            guiGeom = [guiGeom 1];
+            guiGeomV = [guiGeomV 1]; 
+        end
+
+        if contains('Normalize', FitParDefaults)
+            guiElements = [guiElements, ...
+                {{ 'Style', 'checkbox', 'string','Normalize microstate fit' 'tag' 'Normalize','Value',FitPar.Normalize}}];
+            guiGeom = [guiGeom 1];
+            guiGeomV = [guiGeomV 1]; 
+        end
+    end
+
+    % Prompt user to fill in remaining parameters if necessary
+    if ~isempty(guiElements)
+        [res,~,~,outstruct] = inputgui('geometry', guiGeom, 'geomvert', guiGeomV, 'uilist', guiElements,...
+             'title','Microstate fitting parameters');
+
+        if isempty(res)
+            FitPar = [];
+            return; 
+        end
+        
+        if isfield(outstruct, 'nClasses')
+            FitPar.nClasses = PossibleNs(outstruct.nClasses);
+        end
+
+        if isfield(outstruct, 'PeakFit')
+            FitPar.PeakFit = outstruct.PeakFit;
+        end
+
+        if isfield(outstruct, 'BControl')
+            FitPar.BControl = outstruct.BControl;
+        end
+
+        if isfield(outstruct, 'b')
+            FitPar.b = str2double(outstruct.b);
+        end
+
+        if isfield(outstruct, 'lambda')
+            FitPar.lambda = str2double(outstruct.lambda);
+        end
+
+        if isfield(outstruct, 'Rectify')
+            FitPar.Rectify = outstruct.Rectify;
+        end
+
+        if isfield(outstruct, 'Normalize')
+            FitPar.Normalize = outstruct.Normalize;
+        end
+
+        % Re-check FitPar values
+        FitPar = checkFitPar(funcName, PossibleNs, FitPar);
+    end
+
+end
+
+function [FitPar, UsingDefaults] = checkFitPar(funcName, PossibleNs, varargin)
+    
+    if ~isempty(varargin)
+        if isempty(varargin{:})
+            varargin = {};
+        end
+    end
+
+    % Parse and validate inputs
+    p = inputParser;
+    p.FunctionName = funcName;
+    p.KeepUnmatched = true;
+
+    numClass = {'numeric'};
+    numAttributes = {'scalar', 'nonnan'};
+    logClass = {'logical', 'numeric'};
+    logAttributes = {'binary', 'scalar'};
+
+    % Numeric inputs
+    addParameter(p, 'b', 0, @(x) validateattributes(x, numClass, numAttributes, funcName, 'FitPar.b'));
+    addParameter(p, 'lambda', 0.3, @(x) validateattributes(x, numClass, numAttributes, funcName, 'FitPar.lambda'));
+    addParameter(p, 'nClasses', min(PossibleNs), ...
+        @(x) validateattributes(x, numClass, [numAttributes, '>=', min(PossibleNs), '<=', max(PossibleNs)], ...
+        funcName, 'FitPar.nClasses'));
+    
+    % Logical inputs
+    addParameter(p, 'PeakFit', true, @(x) validateattributes(x, logClass, logAttributes, funcName, 'FitPar.PeakFit'));
+    addParameter(p, 'BControl', true, @(x) validateattributes(x, logClass, logAttributes, funcName, 'FitPar.BControl'));
+    addParameter(p, 'Rectify', false, @(x) validateattributes(x, logClass, logAttributes, funcName, 'FitPar.Rectify'));
+    addParameter(p, 'Normalize', false, @(x) validateattributes(x, logClass, logAttributes, funcName, 'FitPar.Normalize'));
+
+    parse(p, varargin{:});
+    FitPar = p.Results;
+    UsingDefaults = p.UsingDefaults;
 end
