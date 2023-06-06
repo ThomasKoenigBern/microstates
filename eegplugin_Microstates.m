a52c019a9f99e5dc156f124a508a658a6ea99a80
delara-aryan <dearyan@chla.usc.edu>
2023-06-06 09:49:03 -0700

    Revert "remove data quality check and outlier detection menu items"
    This reverts commit 213e4b5baaa9bad09916b233d869634c02e951ff.

--- eegplugin_Microstates.m
+++ eegplugin_Microstates.m
@@ -151,12 +151,12 @@ function vers = eegplugin_Microstates (fig, try_strings, catch_strings)
     MSTEMPLATE = MSTemplate;
     
     % Tools menu
-%     comCheckData           = [try_strings.no_check '[~, LASTCOM]                          = pop_CheckData(ALLEEG);'             catch_strings.add_to_hist];
+    comCheckData           = [try_strings.no_check '[~, LASTCOM]                          = pop_CheckData(ALLEEG);'             catch_strings.add_to_hist];
     comFindMSTemplates     = [try_strings.no_check '[EEG, CURRENTSET, LASTCOM]            = pop_FindMSTemplates(ALLEEG);'       catch_strings.store_and_hist];
     comCombineMSTemplates  = [try_strings.no_check '[EEG, LASTCOM]                        = pop_CombMSTemplates(ALLEEG);'       catch_strings.new_and_hist];
     comSortMSTemplates     = [try_strings.no_check '[ALLEEG, EEG, CURRENTSET, LASTCOM]    = pop_SortMSTemplates(ALLEEG);'       catch_strings.store_and_hist];
 %     comDetectOutliers      = [try_strings.no_check '[EEG, CURRENTSET, LASTCOM]            = pop_DetectOutliers(ALLEEG);'        catch_strings.store_and_hist];
-%     comCompareTopos        = [try_strings.no_check '[EEG, CURRENTSET, ~, LASTCOM]         = pop_CompareTopos(ALLEEG);'          catch_strings.store_and_hist];
+    comCompareTopos        = [try_strings.no_check '[EEG, CURRENTSET, ~, LASTCOM]         = pop_CompareTopos(ALLEEG);'          catch_strings.store_and_hist];
     comCompareMaps         = [try_strings.no_check '[EEG, CURRENTSET, LASTCOM]            = pop_CompareMSTemplates(ALLEEG);'    catch_strings.store_and_hist];
     comGetMSDynamics       = [try_strings.no_check '[EEG, CURRENTSET, LASTCOM]            = pop_GetMSDynamics(ALLEEG);'         catch_strings.new_and_hist];
     comFitMSTemplates      = [try_strings.no_check '[EEG, CURRENTSET, LASTCOM]            = pop_FitMSTemplates(ALLEEG);'        catch_strings.store_and_hist];
@@ -170,12 +170,12 @@ function vers = eegplugin_Microstates (fig, try_strings, catch_strings)
     
     toolsmenu = findobj(fig, 'tag', 'tools');
     toolssubmenu = uimenu( toolsmenu, 'label', 'Microstates','userdata','study:on','Separator','on');
-%     uimenu( toolssubmenu, 'Label', 'Data quality check',                                              'CallBack', comCheckData,          'userdata', 'study:on');
+    uimenu( toolssubmenu, 'Label', 'Data quality check',                                              'CallBack', comCheckData,          'userdata', 'study:on');
     uimenu( toolssubmenu, 'Label', 'Identify individual template maps',                               'CallBack', comFindMSTemplates,    'userdata', 'study:on');
     uimenu( toolssubmenu, 'Label', 'Identify group level or grand mean template maps',                'CallBack', comCombineMSTemplates, 'userdata', 'study:on');
     uimenu( toolssubmenu, 'Label', 'Edit & sort template maps',                                       'CallBack', comSortMSTemplates,    'userdata', 'study:on');
 %     uimenu( toolssubmenu, 'Label', 'Outlier detection',                                               'Callback', comDetectOutliers,     'userdata', 'study:on');
-%     uimenu( toolssubmenu, 'Label', 'Compare topographic similarities',                                'CallBack', comCompareTopos,       'userdata', 'study:on');
+    uimenu( toolssubmenu, 'Label', 'Compare topographic similarities',                                'CallBack', comCompareTopos,       'userdata', 'study:on');
     uimenu( toolssubmenu, 'Label', 'Backfit template maps to EEG',                                    'Callback', comFitMSTemplates,     'userdata', 'study:on');
     uimenu( toolssubmenu, 'Label', 'Export temporal parameters',                                      'Callback', comSaveMSParam,        'userdata', 'study:on');
     uimenu( toolssubmenu, 'Label', 'Obtain microstate activation time series (optional)',             'CallBack', comGetMSDynamics,      'userdata', 'study:on', 'Separator', 'on');    
